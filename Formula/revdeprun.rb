81efe219892a61bb389933088349381f33bf207b
Nan Xiao <me@nanx.me>
2026-07-30 22:02:33 -0700

    Update revdeprun to 2.3.1 (#5)

--- Formula/revdeprun.rb
+++ Formula/revdeprun.rb
@@ -1,8 +1,8 @@
 class Revdeprun < Formula
   desc "Reverse dependency checks for R with cloud-ready environment setup"
   homepage "https://nanx.me/revdeprun/"
-  url "https://static.crates.io/crates/revdeprun/revdeprun-2.3.0.crate"
-  sha256 "8e98872565593e39f24373ad46fba83211688e124b70d5dd825664dd9d4783b3"
+  url "https://static.crates.io/crates/revdeprun/revdeprun-2.3.1.crate"
+  sha256 "aeef753f4ac91517ab44eaafbb3d6dd53d7b7a734a4d079cd5532149a7949dcc"
   license "MIT"
   head "https://github.com/nanxstats/revdeprun.git", branch: "main"
 
