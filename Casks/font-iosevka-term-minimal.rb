d32b6e60e41935adfdb42914632a3eb4b24b4cc9
Nan Xiao <me@nanx.me>
2026-08-07 18:51:19 -0700

    Update font-iosevka-term-minimal to 34.8.0 (#7)

--- Casks/font-iosevka-term-minimal.rb
+++ Casks/font-iosevka-term-minimal.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-term-minimal" do
-  version "34.7.0"
-  sha256 "cb75046a641a2a2fb27fc556b44183b4745792ab0f0a9dc450864f7983764321"
+  version "34.8.0"
+  sha256 "bb58292f7f7172d272fb8d4475237469e4e047d5cdebb795ecc80db8b54e615e"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTF-IosevkaTerm-#{version}.zip"
   name "Iosevka Term Minimal"
