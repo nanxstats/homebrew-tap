d40fedc751037dbe3abf64d941dcb00dd3a9930e
Nan Xiao <me@nanx.me>
2026-08-11 15:17:56 -0700

    Update Courier Prime Sans Ligaturized

--- Casks/font-courier-prime-sans-ligaturized.rb
+++ Casks/font-courier-prime-sans-ligaturized.rb
@@ -1,9 +1,9 @@
 cask "font-courier-prime-sans-ligaturized" do
-  version "46532857d80e8f95f3acbb3d9f483647b941cbe5"
+  version "4490f55a87786a2b7c5c8272aba76a2e3719d9ef"
   sha256 :no_check
 
   # A commit archive is intentional: upstream has no tagged releases.
-  url "https://github.com/nanxstats/courier-prime-sans-ligaturized/archive/46532857d80e8f95f3acbb3d9f483647b941cbe5.tar.gz"
+  url "https://github.com/nanxstats/courier-prime-sans-ligaturized/archive/4490f55a87786a2b7c5c8272aba76a2e3719d9ef.tar.gz"
   name "Courier Prime Sans Ligaturized"
   desc "Courier Prime Sans with coding glyphs and Fira Code ligatures"
   homepage "https://github.com/nanxstats/courier-prime-sans-ligaturized"
